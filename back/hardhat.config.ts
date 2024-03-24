5fd7a6993836b01be2f809c7cc166ac5ab0dfd61
liuweiqing <liuweiqing147@gmail.com>
2024-03-24 15:12:44 +0800

    init

--- back/hardhat.config.ts
+++ back/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  // networks: {
+  //   rinkeby: {
+  //     url: "https://rinkeby.infura.io/v3/YOUR-PROJECT-ID",
+  //     accounts: ["YOUR-PRIVATE-KEY"],
+  //   },
+  // },
 };
 
 export default config;
