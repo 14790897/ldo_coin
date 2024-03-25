266671aa01d8b984cd071bfb63d7de98a0c5319f
liuweiqing <liuweiqing147@gmail.com>
2024-03-25 08:52:19 +0800

    init

--- hardhat-boilerplate/contracts/Token.sol
+++ hardhat-boilerplate/contracts/Token.sol
@@ -1,8 +1,12 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+//SPDX-License-Identifier: UNLICENSED
 
-// Uncomment this line to use console.log
+// Solidity files have to start with this pragma.
+// It will be used by the Solidity compiler to validate its version.
+pragma solidity ^0.8.9;
+
+// We import this library to be able to use console.log
 import "hardhat/console.sol";
+
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -10,16 +14,16 @@ contract Token {
     string public name = "My Hardhat Token";
     string public symbol = "MHT";
 
-    // The fixed amount of tokens, stored in an unsigned integer type variable.
+    // The fixed amount of tokens stored in an unsigned integer type variable.
     uint256 public totalSupply = 1000000;
 
     // An address type variable is used to store ethereum accounts.
     address public owner;
 
-    // A mapping is a key/value map. Here we store each account's balance.
+    // A mapping is a key/value map. Here we store each account balance.
     mapping(address => uint256) balances;
 
-    // The Transfer event helps off-chain applications understand
+    // The Transfer event helps off-chain aplications understand
     // what happens within your contract.
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
@@ -36,7 +40,7 @@ contract Token {
     /**
      * A function to transfer tokens.
      *
-     * The `external` modifier makes a function *only* callable from *outside*
+     * The `external` modifier makes a function *only* callable from outside
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
@@ -44,13 +48,16 @@ contract Token {
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
-        // Log the transfer.
+
+        // We can print messages and values using console.log, a feature of
+        // Hardhat Network:
         console.log(
             "Transferring from %s to %s %s tokens",
             msg.sender,
             to,
             amount
         );
+
         // Transfer the amount.
         balances[msg.sender] -= amount;
         balances[to] += amount;
---
+++ hardhat-boilerplate/frontend/src/components/NoWalletDetected.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function NoWalletDetected() {
+  return (
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="col-6 p-4 text-center">
+          <p>
+            No Ethereum wallet was detected. <br />
+            Please install{" "}
+            <a
+              href="https://www.coinbase.com/wallet"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Coinbase Wallet
+            </a>
+            or{" "}
+            <a href="http://metamask.io" target="_blank" rel="noopener noreferrer">
+              MetaMask
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
