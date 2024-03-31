af96bcaa087a95c4eb8162dc6d47c6e654b9fa93
liuweiqing <liuweiqing147@gmail.com>
2024-03-31 13:02:06 +0800

    style: 首页样式

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
-          <Link href="/market">View Task List</Link>
+          {/* <Link href="/market">View Task List</Link> */}
           {/* {isSupabaseConnected && <AuthButton />} */}
         </div>
       </nav>
@@ -45,7 +45,7 @@ export default async function Index() {
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            ldo
+            LinuxDo Token
           </a>
         </p>
       </footer>
